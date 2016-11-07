4caa5d2da89d621aa6d02a452e1e37c1df0155f6
Meghna Prasad Laveti <Meghna Prasad Laveti>
2016-11-06 22:52:55 -0600

    Added the right arn value

--- week10/install-app-env.sh
+++ week10/install-app-env.sh
@@ -2,6 +2,7 @@
 
 export AWS_ACCESS_KEY_ID=AKIAJRP6EG3R5YZE5A7Q
 export AWS_SECRET_ACCESS_KEY=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
+export REGION=us-west-2
 
 aws rds create-db-instance --db-name meghnastorage --db-instance-identifier mpl-db --db-instance-class db.t1.micro --engine MySQL --master-username controller9 --master-user-password meghna999 --allocated-storage 5 --vpc-security-group-ids sg-861163ff --publicly-accessible --port 3306 --allocated-storage 5
 #echo "creating DB may take some time"
@@ -9,9 +10,9 @@ aws rds wait db-instance-available --db-instance-identifier mpl-db
 
 aws sns create-topic --Studentapp
 
-aws sns subscribe --topic-arn arn:aws:iam::131515873733:user/meghnalaveti9 --protocol email --notification-endpoint mlaveti@hawk.iit.edu
+aws sns subscribe --topic-arn arn:null --protocol email --notification-endpoint mlaveti@hawk.iit.edu
 
-aws sqs create-queue --queue-name my-queue --attributes '{ "RedrivePolicy":"{\"deadLetterTargetArn\":\"arn:aws:iam::131515873733:user/meghnalaveti9\", \"maxReceiveCount\":\"5\"}"}'
+aws sqs create-queue --queue-name my-queue --attributes '{ "RedrivePolicy":"{\"deadLetterTargetArn\":\"arn:aws:iam::131515873733:deadLetter\", \"maxReceiveCount\":\"5\"}"}'
 
 aws s3 mb s3://raw-mpl
 
