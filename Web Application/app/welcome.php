4279cf9b9289e6a5486d6d7592cdcaff02592f47
Meghna Prasad Laveti <Meghna Prasad Laveti>
2016-12-03 22:18:13 -0600

    modified files

--- Web Application/app/welcome.php
+++ Web Application/app/welcome.php
@@ -1,4 +1,30 @@
-<?php>
-
-<?>
-welcome.php
+<?php
+session_start();
+$link = mysqli_connect('roundcube.c2amfvktfbda.us-east-1.rds.amazonaws.com', 'roundcube', 'R2oundC8ubE$x','mp1-db') or die('couldnot connect');
+if($_SESSION['userid'] != ''){
+header('Location: welcome.php');
+}
+$username = $_POST['username'];
+$password = $_POST['pwd'];
+if($username != '' && $password != ''){
+$sql = mysqli_query($link,"select * from login where username='".$username."' and password='".$password."'");
+$count = mysqli_num_rows($sql);
+if($count > 0){
+$row = mysqli_fetch_object($sql);
+//echo $row->username;die;
+$id = $row->id;
+$username = $row->username;
+$_SESSION['userid'] = $id;
+$_SESSION['username'] = $username;
+$_SESSION['email'] = $row->email;
+$_SESSION['phone'] = $row->phoneno;
+$_SESSION['role'] = $row->role;
+header('Location: welcome.php');
+}
+else{
+$_SESSION['msg'] = 'Invalid Username/Password';
+}
+}
+else{
+}
+?>
